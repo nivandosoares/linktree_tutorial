83638882f03279d6b16b86fbf8a58e025485b947
Nivando Soares <sidvandoni@gmail.com>
2021-03-19 13:50:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+fetch('https://api.github.com/users/nivandosoares')
+
+.then(response => response.json())
+
+.then(data => {
+
+ 	// console.log(data)
+ 	
+ 	document.getElementById("avatar").src = data.avatar_url;
+ 	document.querySelector('.nome').innerHTML = data.name
+ 	document.querySelector('.short-info').innerHTML = data.bio
+ 	
+     
+
+})
+
+.catch(error => console.error(error))
+
+//alert("burro demais kkkkkkkkk")
+//api_pexels key 563492ad6f91700001000001cd666b8c57c94181aca5492e5fe89981
+//photo id 1257860
+
+
+
+fetch("https://api.pexels.com/v1/photos/1257860",{
+  headers: {
+    Authorization: "563492ad6f91700001000001cd666b8c57c94181aca5492e5fe89981"
+  }
+})
+   .then(resp => {
+     return resp.json()
+   })
+   .then(data => {
+    // console.log(data)
+    document.body.style.backgroundImage = `url(${data.src.original})`
+    
+   })
