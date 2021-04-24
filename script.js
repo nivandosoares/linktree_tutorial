c84f260565ff007db0e530287e558f7df4aba0d0
Nivando Soares <sidvandoni@gmail.com>
2021-04-24 10:56:49 -0300

    async fixes

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ github_user_url = 'https://api.github.com/users/nivandosoares'
 pexels_background_url = 'https://api.pexels.com/v1/photos/1257860'
 pexels_api_key = '563492ad6f91700001000001cd666b8c57c94181aca5492e5fe89981'
 
+async function Get_data_from(url, api_key){
+  try{
+    response = await fetch(url,{
+      Authorization: api_key
+    })
+    return response.json()
+  }catch(error){
+    console.log(error)
+  }
+}
 
+
+/*
 async function Get_GithubUser_from(url){
   try{
     response = await fetch(url)
@@ -10,15 +22,15 @@ async function Get_GithubUser_from(url){
   }catch(error){
     console.log(error)
   }
-}
+}*/
 
-Get_GithubUser_from(github_user_url).then (data => {
+Get_data_from(github_user_url).then (data => {
  //console.log(data)
   document.getElementById("avatar").src = data.avatar_url;
  	document.querySelector('.nome').innerHTML = data.name
  	document.querySelector('.short-info').innerHTML = data.bio
 })
-
+/*
 async function Get_background_from(url){
   try{
     response = await fetch(url, {
@@ -31,8 +43,8 @@ async function Get_background_from(url){
     console.log(error)
   }
 }
-
-Get_background_from(pexels_background_url).then (data => {
+*/
+Get_data_from(pexels_background_url, pexels_api_key).then (data => {
  //console.log(data)
    document.body.style.backgroundImage = `url(${data.src.original})`
     
