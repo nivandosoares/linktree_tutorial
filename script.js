954d3908a5650db799a771b6813e9abb45e83a6b
Nivando Soares <sidvandoni@gmail.com>
2021-04-24 11:17:35 -0300

    added comments

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 github_user_url = 'https://api.github.com/users/nivandosoares'
 photo_id = '1257860'
 pexels_background_url = `https://api.pexels.com/v1/photos/${photo_id}`
-//pexels_api_key = '563492ad6f91700001000001cd666b8c57c94181aca5492e5fe89981'
+
 base64_api_key = 'NTYzNDkyYWQ2ZjkxNzAwMDAxMDAwMDAxY2Q2NjZiOGM1N2M5NDE4MWFjYTU0OTJlNWZlODk5ODE='
+//decoding the base64 api_key to string
 pexels_api_key = atob(base64_api_key)
 //feel free to change these values to make your own linktree clone
-//just get a diferent photo with a diferent id on pexels.com id to make the background and use your github link
+//just get a diferent photo with a diferent id on pexels.com API (see the documentation) to make the background and use your github link
 
 // Set the async function that will make the request using fetch and return a promise
 
@@ -30,8 +31,9 @@ Get_data_from(github_user_url).then (data => {
  	document.querySelector('.short-info').innerHTML = data.bio
 })
 
+//fetch the background from pexels 
 Get_data_from(pexels_background_url, pexels_api_key).then (data => {
-   console.log(data)
+   //set it to the background
    document.body.style.backgroundImage = `url(${data.src.original})`
     
 })
